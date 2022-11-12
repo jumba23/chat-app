18526b697e04f2e83c6f867e40699a6c44fd2100
Goran SD <g.cvetic23@gamail.com>
2022-11-11 17:31:20 -0800

    redux toolkit added and endpoints created

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 fetch API 
 react-bootstrap
+REDUX
 Cloudinary.com - for storing user picture
 socket.io - client
 
---
+++ chat-app-frontend/src/services/appApi.js
@@ -0,0 +1,25 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+
+// define a service user a base URL
+
+const appApi = createApi({
+    reducerPath: 'appApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5001',
+    }),
+
+    endpoints: (builder) => ({
+        // creating the user
+        signupUser: builder.mutation({
+            query: (user) => ({
+                url: "/users",
+                method: "POST",
+                body: user,
+            }),
+        }),
+        // login
+        loginUser: builder.mutation({
+            
+        })
+    }),
+})
