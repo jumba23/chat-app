11ef9135783f13f45b2fd2d08905ea188b1e4587
Goran SD <g.cvetic23@gamail.com>
2022-11-26 19:53:25 -0800

    cors errors addressed

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -19,7 +19,7 @@ require("./connection");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "PORT",
+    origin: "*",
     // origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
