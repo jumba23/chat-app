ee248f3b7fd6fc3fae8843603555a1194882831b
Goran SD <g.cvetic23@gamail.com>
2022-11-28 08:21:19 -0800

    removind server methods on server side

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -17,13 +17,13 @@ app.use("/users", userRoutes);
 require("./connection");
 
 const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: `http://${PORT}`,
-    // origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+const io = require("socket.io")(server) 
+//   {
+//   cors: {
+//     origin: `http://${PORT}`,
+//     methods: ["GET", "POST"],
+//   },
+// });
 
 app.get("/rooms", (req, res) => {
   res.json(rooms);
